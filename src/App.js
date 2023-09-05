b07ea3a857c837aba36581e29e5879a229fa3168
Clement Hannicq <clementh@theodo.fr>
2023-09-05 13:25:36 +0200

    Save the current graph and config in the url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import JSONCrush from 'jsoncrush';
 
 import Process from './Process';
 import Config from './Config';
@@ -23,18 +24,51 @@ const INITIAL_CONFIG = {
 function App() {
   const [steps, setSteps] = useState(INITIAL_VALUE);
   const [config, setConfig] = useState(INITIAL_CONFIG);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    try {
+      const param = decodeURIComponent(document.location.hash.replace(/^#/, ""));
+      if (param !== '') {
+        const { s, c } = JSON.parse(JSONCrush.uncrush(param));
+        setSteps(s.map(step => ({...step, id: Math.random()})));
+        setConfig(oldConfig => ({...oldConfig, ...c}))
+      }
+    } catch (e) {
+      console.error("could not load saved state");
+      console.error(e);
+    }
+    setLoaded(true);
+  }, []);
+
+  useEffect(() => {
+    if (!loaded) {
+      return;
+    }
+    const stepsToSave = steps.map(({ text, color }) => ({ text, color }));
+    const configToSave = Object.keys(config).reduce((acc, key) => {
+      if(config[key] !== INITIAL_CONFIG[key]) {
+        return {...acc, [key]: config[key]};
+      }
+      return acc;
+    }, {});
+    const param = JSONCrush.crush(JSON.stringify({s: stepsToSave, c: configToSave }));
+    document.location.hash = encodeURIComponent(param);
+  }, [steps, config, loaded]);
 
   return (
     <DndProvider backend={HTML5Backend}>
-      <Process steps={steps} config={config} />
-      <section className="config">
-        <div className="config--panel">
-          <Editor config={config} steps={steps} setSteps={setSteps} />
-        </div>
-        <div className="config--panel">
-          <Config config={config} setConfig={setConfig} />
-        </div>
-      </section>
+      { loaded && <>
+        <Process steps={steps} config={config} />
+        <section className="config">
+          <div className="config--panel">
+            <Editor config={config} steps={steps} setSteps={setSteps} />
+          </div>
+          <div className="config--panel">
+            <Config config={config} setConfig={setConfig} />
+          </div>
+        </section>
+      </>}
     </DndProvider>
   );
 }
